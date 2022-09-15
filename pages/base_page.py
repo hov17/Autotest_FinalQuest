b2da8f37ec042e1ffe1b18f3cbcc36a014de2a7a
hov17 <demix555@mail.ru>
2022-09-15 11:34:10 +0300

    Добавление первого теста на основе Page Object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
 class BasePage():
 
     # конструктор - метод, который вызывается, когда мы создаем объект
-    def __int__(self, browser, url):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
