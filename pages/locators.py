64f77e9069b84ceafb45e6abe9c06f0e610036db
hov17 <demix555@mail.ru>
2022-09-16 10:21:40 +0300

    Добавление локаторов для login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+    REGISTRATION_FROM = (By.ID, 'register_form')
+    LOGIN_FORM = (By.ID, 'login_from')
