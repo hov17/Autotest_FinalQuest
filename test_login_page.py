a654475d4391375c324766abcce7d80d3fcf28a5
hov17 <demix555@mail.ru>
2022-09-16 14:13:18 +0300

    Тест в файле test_main_page.py

--- test_login_page.py
+++
@@ -1,8 +0,0 @@
-from .pages.login_page import LoginPage
-
-
-def test_should_be_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
-    page = LoginPage(browser, link, timeout=10)
-    page.open()
-    page.should_be_login_page()
