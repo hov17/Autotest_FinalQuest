b1c799f0cf6555c0b9c050c8552a0404d8b43507
hov17 <demix555@mail.ru>
2022-09-16 17:13:18 +0300

    Добавление локатаров для страницы product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,14 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTRATION_FROM = (By.ID, 'register_form')
     LOGIN_FORM = (By.ID, 'login_form')
+
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    MESSAGE_OF_SUCCESS_ADDED_BOOK = (By.CSS_SELECTOR, '#messages :nth-child(1).alert')
+    MESSAGE_OF_SUCCESS_ADDED_BOOK_WITH_PRICE = (By.CSS_SELECTOR, '#messages :nth-child(1).alert')
+    NAME_OF_BOOK = (By.CSS_SELECTOR, '#content_inner h1')
+    NAME_OF_BOOK_IN_MESSAGE = (By.CSS_SELECTOR, '#messages :nth-child(1) > .alertinner strong')
+    PRICE_OF_BOOK = (By.CSS_SELECTOR, '#content_inner p')
+    PRICE_OF_BOOK_IN_MESSAGE = (By.CSS_SELECTOR, '#content_inner .product_main .price_color')
+
