6fed9909ed59fbbde850b2f15908d6801bd71ec8
hov17 <demix555@mail.ru>
2022-09-19 10:55:19 +0300

    Добавление разных страниц промо для теста добавления товара в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#  В параметрах указываем различные промо. 7-ое промо заведомо падающее
+@pytest.mark.parametrize('num', [*range(7), pytest.param(7, marks=pytest.mark.xfail(reason='Bugged')),
+                                 *range(8, 10)])
+# Тест на добавление товара в корзину и проверку сообщений
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
     page = ProductPage(browser, link, timeout=5)
     page.open()
     page.product_add_to_basket()
