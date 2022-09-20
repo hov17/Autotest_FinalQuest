9da8e89b8b66c382356a3f28fa1aab012f25b462
hov17 <demix555@mail.ru>
2022-09-20 13:17:47 +0300

    Объединение логин тестов в один класс

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,26 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-# Проверка перехода на страницу авторизации
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link, timeout=10)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    # Проверка перехода на страницу авторизации
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link, timeout=10)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-
-# # Проверка на наличии кнопки перехода на страницу авторизации
-def test_guest_should_be_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link, timeout=10)
-    page.open()
-    page.should_be_login_link()
+    # Проверка на наличии кнопки перехода на страницу авторизации
+    def test_guest_should_be_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link, timeout=10)
+        page.open()
+        page.should_be_login_link()
 
 
 # Переход в пустую корзину с главной страницы
@@ -30,5 +32,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url, timeout=10)
     basket_page.basket_should_be_empty()
     basket_page.should_be_empty_basket_message()
-
-
